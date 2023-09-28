483d2ddda55fb781a5d9820a5810248ae6917f29
AminEl93 <amin_11_iverson@hotmail.com>
2023-09-28 02:11:07 +0200

    Componente de la card del gif

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -25,7 +25,7 @@ export class GifsService {
 
     private organizeHistory(tag: string) {
         tag = tag.toLowerCase();
-        if (this._tagsHistory.includes(tag)) {
+        if(this._tagsHistory.includes(tag)) {
             this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
         }    
         this._tagsHistory.unshift(tag);
@@ -40,12 +40,12 @@ export class GifsService {
     private loadLocalStorage(): void {
         if(!localStorage.getItem('history')) return;
         this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
-        if (this._tagsHistory.length === 0) return;
+        if(this._tagsHistory.length === 0) return;
         this.searchTag(this._tagsHistory[0]);
     }
 
     searchTag(tag: string): void {
-        if (tag.length === 0) return;
+        if(tag.length === 0) return;
         this.organizeHistory(tag);
 
         const params = new HttpParams()
